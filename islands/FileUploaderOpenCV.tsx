eda35d71a039cfcb9ff2198187b10d3335e8b7e6
hiroki.goto <blackjiro46248@gmail.com>
2023-02-20 23:44:32 +0900

    修正

--- islands/FileUploaderOpenCV.tsx
+++ islands/FileUploaderOpenCV.tsx
@@ -4,18 +4,19 @@ import { useEffect, useState } from "preact/hooks";
 declare global {
   interface Window {
     cv: any;
+    Utils: any;
   }
 }
 
-const { cv } = window;
+const { cv, Utils } = window;
 
 export default function FileUploaderOpenCV() {
   const [file, setFile] = useState<File | null>(null);
   const [imageSrc, setImageSrc] = useState<string>();
 
   useEffect(() => {
-    let utils = new Utils("errorMessage");
-    let faceCascadeFile = "haarcascade_frontalface_default.xml";
+    const utils = new Utils("errorMessage");
+    const faceCascadeFile = "haarcascade_frontalface_default.xml";
     utils.createFileFromUrl(faceCascadeFile, faceCascadeFile, () => {});
   }, []);
 
@@ -29,19 +30,19 @@ export default function FileUploaderOpenCV() {
     if (!file) return;
 
     const imgElement = document.getElementById("imageSrc");
-    let src = cv.imread(imgElement);
-    let gray = new cv.Mat();
+    const src = cv.imread(imgElement);
+    const gray = new cv.Mat();
     cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
-    let faces = new cv.RectVector();
-    let faceCascade = new cv.CascadeClassifier();
+    const faces = new cv.RectVector();
+    const faceCascade = new cv.CascadeClassifier();
     // load pre-trained classifiers
     faceCascade.load("haarcascade_frontalface_default.xml");
     // detect faces
-    let msize = new cv.Size(0, 0);
-    faceCascade.detectMultiScale(gray, faces, 1.1, 3, 0, msize, msize);
+    const msize = new cv.Size(0, 0);
+    faceCascade.detectMultiScale(gray, faces, 1.2, 3, 0, msize, msize);
     for (let i = 0; i < faces.size(); ++i) {
-      let point1 = new cv.Point(faces.get(i).x, faces.get(i).y);
-      let point2 = new cv.Point(
+      const point1 = new cv.Point(faces.get(i).x, faces.get(i).y);
+      const point2 = new cv.Point(
         faces.get(i).x + faces.get(i).width,
         faces.get(i).y + faces.get(i).height,
       );
@@ -64,22 +65,23 @@ export default function FileUploaderOpenCV() {
       />
       <div class="flex mt-3 gap-2">
         <button class="btn btn-primary flex-1" onClick={loadImage}>
-          ブラウザ上で切り抜き
+          顔検知
         </button>
-        <button class="btn btn-primary flex-1">APIで切り抜き</button>
       </div>
-      <div class="flex justify-center mt-6">
-        {imageSrc && (
-          <img
-            src={imageSrc}
-            alt="face"
-            width="300"
-            id="imageSrc"
-          />
-        )}
-      </div>
-      <div class="flex justify-center mt-6">
-        <canvas id="canvasOutput"></canvas>
+      <div class="flex justify-around gap-3">
+        <div class="flex justify-center mt-6">
+          {imageSrc && (
+            <img
+              src={imageSrc}
+              alt="face"
+              width="300"
+              id="imageSrc"
+            />
+          )}
+        </div>
+        <div class="flex justify-center mt-6">
+          <canvas id="canvasOutput"></canvas>
+        </div>
       </div>
       <div id="errorMessage"></div>
     </div>
